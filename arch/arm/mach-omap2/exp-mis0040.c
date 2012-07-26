fabc2ace43a63b18508a890c23099595a837f880
Robert Woerle <robert@linuxdevelopment.de>
2012-07-26 09:03:21 +0200

    fix comment on rev of board

--- arch/arm/mach-omap2/exp-mis0040.c
+++ arch/arm/mach-omap2/exp-mis0040.c
@@ -4,7 +4,7 @@
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
- * 7" TOUCH 7aw Panel 
+ * 7" TOUCH T07BA Panel 
  */
 
 #include <linux/kernel.h>
