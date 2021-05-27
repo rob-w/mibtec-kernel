bd3311b61bed968a6f643bf7594c8637bd6b222b
Robert Woerle <robert@linuxdevelopment.de>
2021-05-27 13:18:19 +0200

    remove obsolete .h and initial state working
    Signed-off-by: Robert Woerle <robert@linuxdevelopment.de>

--- drivers/gpio/gpio-si838x.c
+++ drivers/gpio/gpio-si838x.c
@@ -1,192 +1,226 @@
 /*
- * si838x spi based bipolar input controller
- * Copyright (c) 2021 Mibtec.de
- * 
- * base on drivers/gpio/gpio-mc33880.c
- * Copyright (c) 2009 Intel Corporation
+ * Copyright (c) 2021 MIBTEC.de robert@linuxdevelopment,de
+ *  base on drivers/gpio/gpio-pisosr.c
+ * Copyright (C) 2015-2016 Texas Instruments Incorporated - http://www.ti.com/
+ *	Andrew F. Davis <afd@ti.com>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * This program is distributed "as is" WITHOUT ANY WARRANTY of any
+ * kind, whether expressed or implied; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License version 2 for more details.
  */
 
-/* Supports:
- * Silicon Labs 838x bipolar input controller
- */
-
-#include <linux/init.h>
-#include <linux/mutex.h>
-#include <linux/spi/spi.h>
-#include <linux/spi/si838x.h>
-#include <linux/gpio.h>
-#include <linux/slab.h>
+#include <linux/delay.h>
+#include <linux/gpio/consumer.h>
+#include <linux/gpio/driver.h>
 #include <linux/module.h>
+#include <linux/mutex.h>
+#include <linux/of.h>
+#include <linux/spi/spi.h>
 
-#define DRIVER_NAME "si838x"
+#define SI838X_MODULE_VERSION "0.1"
 
-/*
- * Pin configurations, see MAX7301 datasheet page 6
+#define DEFAULT_NGPIO 8
+
+/**
+ * struct pisosr_gpio - GPIO driver data
+ * @chip: GPIO controller chip
+ * @spi: SPI device pointer
+ * @buffer: Buffer for device reads
+ * @buffer_size: Size of buffer
+ * @lock: Protects read sequences
  */
-#define PIN_CONFIG_MASK 0x00
-#define PIN_CONFIG_IN_PULLUP 0x00
-#define PIN_CONFIG_IN_WO_PULLUP 0x00
-#define PIN_CONFIG_OUT 0x00
-
-#define PIN_NUMBER 8
-
-
-/*
- * Some registers must be read back to modify.
- * To save time we cache them here in memory
- */
-struct si838x {
-	struct mutex	lock;	/* protect from simultaneous accesses */
-	u8		port_config;
+struct si838x_gpio {
 	struct gpio_chip chip;
 	struct spi_device *spi;
+	u8 *buffer;
+	size_t buffer_size;
+	struct mutex lock;
 };
 
-static int si838x_write_config(struct si838x *mc)
+static inline struct si838x_gpio *to_si838x_gpio(struct gpio_chip *chip)
 {
-	return spi_write(mc->spi, &mc->port_config, sizeof(mc->port_config));
+	return container_of(chip, struct si838x_gpio, chip);
 }
 
+#define SI838x_ADR_CHAN_STATUS		0x0
+#define SI838x_ADR_DBNC_MODE0		0x1
+#define SI838x_ADR_DBNC_MODE1		0x2
+#define SI838x_ADR_DBNC_DLY0		0x3
+#define SI838x_ADR_DBNC_DLY1		0x4
 
-static int __si838x_set(struct si838x *mc, unsigned offset, int value)
+#define SI838x_BRCAT			(1<<7)
+#define SI838x_READ_EN			(1<<6)
+
+#define SI838x_M_DEGLITCH		0x0
+#define SI838x_M_LOWPASS		0x1
+#define SI838x_M_BLANKING		0x2
+
+#define SI838x_D_BOUNCE_0_MS		0x0
+#define SI838x_D_BOUNCE_10_MS		0x1
+#define SI838x_D_BOUNCE_30_MS		0x2
+#define SI838x_D_BOUNCE_100_MS		0x3
+
+static int si838x_gpio_set_debounce(struct gpio_chip *chip, unsigned offset, unsigned debounce)
 {
-	if (value)
-		mc->port_config |= 1 << offset;
-	else
-		mc->port_config &= ~(1 << offset);
-
-	return si838x_write_config(mc);
-}
-
-
-static void si838x_set(struct gpio_chip *chip, unsigned offset, int value)
-{
-	struct si838x *mc = container_of(chip, struct si838x, chip);
-
-	mutex_lock(&mc->lock);
-
-	__si838x_set(mc, offset, value);
-
-	mutex_unlock(&mc->lock);
-}
-
-static int si838x_probe(struct spi_device *spi)
-{
-	struct si838x *mc;
-	struct si838x_platform_data *pdata;
 	int ret;
+	u8 tx_buf[3];
+	struct si838x_gpio *gpio = to_si838x_gpio(chip);
 
-	pdata = dev_get_platdata(&spi->dev);
-	if (!pdata || !pdata->base) {
-		dev_dbg(&spi->dev, "incorrect or missing platform data\n");
-		return -EINVAL;
-	}
+	mutex_lock(&gpio->lock);
 
-	/*
-	 * bits_per_word cannot be configured in platform data
-	 */
-	spi->bits_per_word = 8;
+	tx_buf[0] = SI838x_BRCAT;
+	tx_buf[1] = SI838x_ADR_DBNC_MODE0;
 
-	ret = spi_setup(spi);
-	if (ret < 0)
+	ret = spi_write(gpio->spi, &tx_buf, sizeof(tx_buf));
+	if (ret)
 		return ret;
 
-	mc = devm_kzalloc(&spi->dev, sizeof(struct si838x), GFP_KERNEL);
-	if (!mc)
-		return -ENOMEM;
+	dev_info(gpio->chip.dev, "%s() buf %d %d\n", __func__, tx_buf[0], tx_buf[1]);
 
-	mutex_init(&mc->lock);
-
-	spi_set_drvdata(spi, mc);
-
-	mc->spi = spi;
-
-	mc->chip.label = DRIVER_NAME,
-	mc->chip.set = si838x_set;
-	mc->chip.base = pdata->base;
-	mc->chip.ngpio = PIN_NUMBER;
-	mc->chip.can_sleep = true;
-	mc->chip.dev = &spi->dev;
-	mc->chip.owner = THIS_MODULE;
-
-	mc->port_config = 0x00;
-	/* write twice, because during initialisation the first setting
-	 * is just for testing SPI communication, and the second is the
-	 * "real" configuration
-	 */
-	ret = si838x_write_config(mc);
-	mc->port_config = 0x00;
-	if (!ret)
-		ret = si838x_write_config(mc);
-
-	if (ret) {
-		dev_err(&spi->dev, "Failed writing to " DRIVER_NAME ": %d\n",
-			ret);
-		goto exit_destroy;
-	}
-
-	ret = gpiochip_add(&mc->chip);
-	if (ret)
-		goto exit_destroy;
-
-	return ret;
-
-exit_destroy:
-	mutex_destroy(&mc->lock);
-	return ret;
-}
-
-static int si838x_remove(struct spi_device *spi)
-{
-	struct si838x *mc;
-
-	mc = spi_get_drvdata(spi);
-	if (!mc)
-		return -ENODEV;
-
-	gpiochip_remove(&mc->chip);
-	mutex_destroy(&mc->lock);
+	mutex_unlock(&gpio->lock);
 
 	return 0;
 }
 
-static struct spi_driver si838x_driver = {
-	.driver = {
-		.name		= DRIVER_NAME,
-		.owner		= THIS_MODULE,
-	},
-	.probe		= si838x_probe,
-	.remove		= si838x_remove,
+static int si838x_gpio_refresh(struct si838x_gpio *gpio)
+{
+	int ret;
+	u8 tx_buf[2];
+
+	mutex_lock(&gpio->lock);
+
+	tx_buf[0] = 0x40;
+	tx_buf[1] = 0x0;
+
+	ret = spi_write_then_read(gpio->spi, &tx_buf, sizeof(tx_buf), gpio->buffer, gpio->buffer_size);
+	if (ret)
+		return ret;
+
+	dev_info(gpio->chip.dev, "%s() buf %d\n", __func__,  gpio->buffer[0]);
+
+	mutex_unlock(&gpio->lock);
+
+	return 0;
+}
+
+static int si838x_gpio_get_direction(struct gpio_chip *chip,
+				     unsigned offset)
+{
+	/* This device always input */
+	return 1;
+}
+
+static int si838x_gpio_direction_input(struct gpio_chip *chip,
+				       unsigned offset)
+{
+	/* This device always input */
+	return 0;
+}
+
+static int si838x_gpio_direction_output(struct gpio_chip *chip,
+					unsigned offset, int value)
+{
+	/* This device is input only */
+	return -EINVAL;
+}
+
+static int si838x_gpio_get(struct gpio_chip *chip, unsigned offset)
+{
+	struct si838x_gpio *gpio = to_si838x_gpio(chip);
+
+	si838x_gpio_refresh(gpio);
+
+	return (gpio->buffer[offset / 8] >> (offset % 8)) & 0x1;
+}
+
+static struct gpio_chip template_chip = {
+	.label			= "si838x-gpio",
+	.owner			= THIS_MODULE,
+	.get_direction		= si838x_gpio_get_direction,
+	.direction_input	= si838x_gpio_direction_input,
+	.direction_output	= si838x_gpio_direction_output,
+	.get			= si838x_gpio_get,
+	.set_debounce	= si838x_gpio_set_debounce,
+	.base			= -1,
+	.ngpio			= DEFAULT_NGPIO,
+	.can_sleep		= true,
 };
 
-static int __init si838x_init(void)
+static int si838x_gpio_probe(struct spi_device *spi)
 {
-	return spi_register_driver(&si838x_driver);
-}
-/* register after spi postcore initcall and before
- * subsys initcalls that may rely on these GPIOs
- */
-subsys_initcall(si838x_init);
+	struct device *dev = &spi->dev;
+	struct si838x_gpio *gpio;
+	int ret;
 
-static void __exit si838x_exit(void)
+	gpio = devm_kzalloc(dev, sizeof(*gpio), GFP_KERNEL);
+	if (!gpio)
+		return -ENOMEM;
+
+	spi_set_drvdata(spi, gpio);
+
+	gpio->chip = template_chip;
+	gpio->chip.dev = dev;
+	of_property_read_u16(dev->of_node, "ngpios", &gpio->chip.ngpio);
+
+	gpio->spi = spi;
+
+	gpio->buffer_size = DIV_ROUND_UP(gpio->chip.ngpio, 8);
+	gpio->buffer = devm_kzalloc(dev, gpio->buffer_size, GFP_KERNEL);
+	if (!gpio->buffer)
+		return -ENOMEM;
+
+	mutex_init(&gpio->lock);
+
+	dev_info(dev, "v%s\n", SI838X_MODULE_VERSION);
+
+	ret = gpiochip_add(&gpio->chip);
+	if (ret < 0) {
+		dev_err(dev, "Unable to register gpiochip\n");
+		return ret;
+	}
+
+	return 0;
+}
+
+static int si838x_gpio_remove(struct spi_device *spi)
 {
-	spi_unregister_driver(&si838x_driver);
-}
-module_exit(si838x_exit);
+	struct si838x_gpio *gpio = spi_get_drvdata(spi);
 
-MODULE_AUTHOR("Mocean Laboratories <info@mocean-labs.com>");
+	gpiochip_remove(&gpio->chip);
+
+	mutex_destroy(&gpio->lock);
+
+	return 0;
+}
+
+static const struct spi_device_id si838x_gpio_id_table[] = {
+	{ "si838x-gpio", },
+	{ /* sentinel */ }
+};
+MODULE_DEVICE_TABLE(spi, si838x_gpio_id_table);
+
+static const struct of_device_id si838x_gpio_of_match_table[] = {
+	{ .compatible = "si838x-gpio", },
+	{ /* sentinel */ }
+};
+MODULE_DEVICE_TABLE(of, si838x_gpio_of_match_table);
+
+static struct spi_driver si838x_gpio_driver = {
+	.driver = {
+		.name = "si838x-gpio",
+		.of_match_table = si838x_gpio_of_match_table,
+	},
+	.probe = si838x_gpio_probe,
+	.remove = si838x_gpio_remove,
+	.id_table = si838x_gpio_id_table,
+};
+module_spi_driver(si838x_gpio_driver);
+
+MODULE_AUTHOR("Robert Woerle <robert@linuxdevelopment.de>");
+MODULE_DESCRIPTION("SPI Compatible SI838x INPUT GPIO Driver");
 MODULE_LICENSE("GPL v2");
-
