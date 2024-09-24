944842ec08144334d90d3e602f26bd474e724712
Robert Wörle <rwoerle@mibtec.de>
2024-09-24 14:39:00 +0200

    add missing dts file, extend d880tsm92 for new board
    Signed-off-by: Robert Wörle <rwoerle@mibtec.de>

---
+++ include/dt-bindings/board/am335x-bbw-bbb-base.h
@@ -0,0 +1,108 @@
+/*
+ * This header provides constants for bbw/bbb pinctrl bindings.
+ *
+ * Copyright (C) 2014 Robert Nelson <robertcnelson@gmail.com>
+ *
+ * Numbers Based on: https://github.com/derekmolloy/boneDeviceTree/tree/master/docs
+ */
+
+#ifndef _DT_BINDINGS_BOARD_AM335X_BBW_BBB_BASE_H
+#define _DT_BINDINGS_BOARD_AM335X_BBW_BBB_BASE_H
+
+#define BONE_P8_03 0x818
+#define BONE_P8_04 0x81C
+
+#define BONE_P8_05 0x808
+#define BONE_P8_06 0x80C
+#define BONE_P8_07 0x890
+#define BONE_P8_08 0x894
+
+#define BONE_P8_09 0x89C
+#define BONE_P8_10 0x898
+#define BONE_P8_11 0x834
+#define BONE_P8_12 0x830
+
+#define BONE_P8_13 0x824
+#define BONE_P8_14 0x828
+#define BONE_P8_15 0x83C
+#define BONE_P8_16 0x838
+
+#define BONE_P8_17 0x82C
+#define BONE_P8_18 0x88C
+#define BONE_P8_19 0x820
+#define BONE_P8_20 0x884
+
+#define BONE_P8_21 0x880
+#define BONE_P8_22 0x814
+#define BONE_P8_23 0x810
+#define BONE_P8_24 0x804
+
+#define BONE_P8_25 0x800
+#define BONE_P8_26 0x87C
+#define BONE_P8_27 0x8E0
+#define BONE_P8_28 0x8E8
+
+#define BONE_P8_29 0x8E4
+#define BONE_P8_30 0x8EC
+#define BONE_P8_31 0x8D8
+#define BONE_P8_32 0x8DC
+
+#define BONE_P8_33 0x8D4
+#define BONE_P8_34 0x8CC
+#define BONE_P8_35 0x8D0
+#define BONE_P8_36 0x8C8
+
+#define BONE_P8_37 0x8C0
+#define BONE_P8_38 0x8C4
+#define BONE_P8_39 0x8B8
+#define BONE_P8_40 0x8BC
+
+#define BONE_P8_41 0x8B0
+#define BONE_P8_42 0x8B4
+#define BONE_P8_43 0x8A8
+#define BONE_P8_44 0x8AC
+
+#define BONE_P8_45 0x8A0
+#define BONE_P8_46 0x8A4
+
+#define BONE_P9_11 0x870
+#define BONE_P9_12 0x878
+
+#define BONE_P9_13 0x874
+#define BONE_P9_14 0x848
+#define BONE_P9_15 0x840
+#define BONE_P9_16 0x84C
+
+#define BONE_P9_17 0x95C
+#define BONE_P9_18 0x958
+#define BONE_P9_19 0x97C
+#define BONE_P9_20 0x978
+
+#define BONE_P9_21 0x954
+#define BONE_P9_22 0x950
+#define BONE_P9_23 0x844
+#define BONE_P9_24 0x984
+
+#define BONE_P9_25 0x9AC
+#define BONE_P9_26 0x980
+#define BONE_P9_27 0x9A4
+#define BONE_P9_28 0x99C
+
+#define BONE_P9_29 0x994
+#define BONE_P9_30 0x998
+#define BONE_P9_31 0x990
+
+/* Shared P21 of P11 */
+#define BONE_P9_41 0x9B4
+#define BONE_P9_41A 0x9B4
+#define BONE_P9_41B 0x9A8
+#define BONE_P9_91 0x9A8
+
+/* Shared P22 of P11 */
+#define BONE_P9_42 0x964
+#define BONE_P9_42A 0x964
+#define BONE_P9_42B 0x9A0
+#define BONE_P9_92 0x9A0
+
+#endif
+
